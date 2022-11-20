35fdf31b02cc40a804ddc139760938b36df0c1fc
I-am-Anton-D <motohd@yandex.ru>
2022-11-20 15:51:50 +0300

    fix convertValue, add default dimension

--- src/main/kotlin/unit/prototype/MetricUnit.kt
+++ src/main/kotlin/unit/prototype/MetricUnit.kt
@@ -2,8 +2,13 @@ package unit.prototype
 
 import dimension.Dimension
 import unit.Prefix
+import java.math.BigDecimal
 
 abstract class MetricUnit<Q> : AbstractUnit<Q>() {
-    fun pow(pow: Int = 1, prefix: Prefix) = Dimension(this, pow, prefix)
-    fun prefix(prefix: Prefix) =  Dimension(this, 1, prefix)
+
+    open fun pow(pow: Int = 1, prefix: Prefix) = Dimension(this, pow, prefix)
+
+    open fun prefix(prefix: Prefix) =  Dimension(this, 1, prefix)
+
+    open fun valueToBaseUnit(number: Number, prefix: Prefix): BigDecimal = prefix.inNominal(number).multiply(ratio)
  }
